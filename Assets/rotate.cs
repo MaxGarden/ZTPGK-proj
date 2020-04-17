0d127113e45e70882bf770048201c9153d0b867a
karo1404 <karol1404@vp.pl>
2020-04-17 16:00:35 +0200

    fireworks prefab

---
+++ Assets/rotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 5.0f, 0, Space.Self);
+        transform.Translate(Vector3.up * (float)System.Math.Sin((double)Time.frameCount)*20.0f, Space.Self);
+    }
+}
