681cde0da8ad8f4a3a921595f7f77c1b25916431
karo1404 <karol1404@vp.pl>
2020-04-17 15:28:37 +0200

    changed fire controller

--- Assets/Scripts/Controllers/FireController.cs
+++ Assets/Scripts/Controllers/FireController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEditor;
 
 [RequireComponent(typeof(Collider))]
 public sealed class FireController : MonoBehaviour
@@ -10,21 +11,38 @@ public sealed class FireController : MonoBehaviour
     private ParticleSystem m_smokeParticleSystem;
 
     private int m_triggersCount;
-    
+
+    private SerializedObject flamePart;
+
     private void Awake()
     {
         m_flameParticleSystem.Play();
         m_smokeParticleSystem.Stop();
+        flamePart = new SerializedObject(m_flameParticleSystem);
     }
 
     private void OnTriggerEnter(Collider other)
     {
+
+        flamePart.FindProperty("ShapeModule.angle").floatValue = 75.0f;
+        flamePart.ApplyModifiedProperties();
+        m_flameParticleSystem.Emit(200);
+
+        flamePart.FindProperty("EmissionModule.rateOverTime.scalar").floatValue = 10.0f;
+        flamePart.ApplyModifiedProperties();
+
         if (m_triggersCount++ <= 0)
             m_smokeParticleSystem.Play();
     }
 
     private void OnTriggerExit(Collider other)
     {
+
+         flamePart.FindProperty("ShapeModule.angle").floatValue = 0.0f;
+         flamePart.FindProperty("EmissionModule.rateOverTime.scalar").floatValue = 400.0f;
+         flamePart.ApplyModifiedProperties();
+
+
         if (--m_triggersCount <= 0)
             m_smokeParticleSystem.Stop();
     }
