d3fa1dfa7ce53379cfadf2f863d8c6288145645e
Mateusz Serkies <mateusz.serkies@artifexmundi.com>
2020-04-17 13:22:08 +0200

    Added fire

---
+++ Assets/Scripts/Controllers/FireController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public sealed class FireController : MonoBehaviour
+{
+    [SerializeField]
+    private ParticleSystem m_flameParticleSystem;
+
+    [SerializeField]
+    private ParticleSystem m_smokeParticleSystem;
+
+    private int m_triggersCount;
+    
+    private void Awake()
+    {
+        m_flameParticleSystem.Play();
+        m_smokeParticleSystem.Stop();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (m_triggersCount++ <= 0)
+            m_smokeParticleSystem.Play();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (--m_triggersCount <= 0)
+            m_smokeParticleSystem.Stop();
+    }
+}
+
